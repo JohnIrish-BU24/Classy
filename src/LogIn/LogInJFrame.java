10fbca05c4f603c3ed3a382f12ad13d31cbea364
rheanicolelumbes <rheanicolellona.lumbes24@bicol-u.edu.ph>
2025-12-28 22:08:08 +0800

    Config: Log In Interface

--- src/LogIn/LogInJFrame.java
+++ src/LogIn/LogInJFrame.java
@@ -17,6 +17,46 @@ public class LogInJFrame extends javax.swing.JFrame {
      */
     public LogInJFrame() {
         initComponents();
+        // Replace these numbers with the ones you found in Properties!
+        int width = 1000;   // <--- CHANGE THIS. If you have space on the right, make this number smaller.
+        int height = 567;  // <--- Keep this matching your image height.
+
+        this.setSize(width, height);
+        // 1. Center the window on the screen (Professional touch)
+        this.setLocationRelativeTo(null);
+
+        // 2. Prevent the window from being resized larger than the image
+        this.setResizable(false);
+        
+        // --- RESIZE USER ICON (Small) ---
+        // Change 'jLabel3' to your actual User Icon variable name
+        javax.swing.ImageIcon userIcon = (javax.swing.ImageIcon) jLabel3.getIcon();
+        if (userIcon != null) {
+            java.awt.Image img = userIcon.getImage();
+            // Resize to 30x30 pixels (Standard icon size)
+            java.awt.Image newImg = img.getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
+            jLabel3.setIcon(new javax.swing.ImageIcon(newImg));
+        }
+
+        // --- RESIZE PASSWORD ICON (Small) ---
+        // Change 'jLabel4' to your actual Password Icon variable name
+        javax.swing.ImageIcon passIcon = (javax.swing.ImageIcon) jLabel4.getIcon();
+        if (passIcon != null) {
+            java.awt.Image img = passIcon.getImage();
+            // Resize to 30x30 pixels
+            java.awt.Image newImg = img.getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
+            jLabel4.setIcon(new javax.swing.ImageIcon(newImg));
+        }
+
+        // --- RESIZE CLASSY LOGO (Medium) ---
+        // Change 'jLabel2' to your actual Logo variable name
+        javax.swing.ImageIcon logoIcon = (javax.swing.ImageIcon) jLabel5.getIcon();
+        if (logoIcon != null) {
+            java.awt.Image img = logoIcon.getImage();
+            // Resize to 200 width, 80 height (Adjust these numbers to fit nicely)
+            java.awt.Image newImg = img.getScaledInstance(275, 100, java.awt.Image.SCALE_SMOOTH);
+            jLabel5.setIcon(new javax.swing.ImageIcon(newImg));
+        }
     }
 
     /**
@@ -28,27 +68,112 @@ public class LogInJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel5 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/LogIn/Classy Logo.png"))); // NOI18N
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 40, -1, -1));
+
+        jPanel1.setBackground(new java.awt.Color(255, 235, 205));
+
+        jLabel2.setFont(new java.awt.Font("Tw Cen MT", 1, 24)); // NOI18N
+        jLabel2.setText("User Login");
+
+        jTextField1.addActionListener(this::jTextField1ActionPerformed);
+
+        jPasswordField1.addActionListener(this::jPasswordField1ActionPerformed);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/LogIn/User Logo.png"))); // NOI18N
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/LogIn/Logo (3).png"))); // NOI18N
+
+        jCheckBox1.setFont(new java.awt.Font("Tw Cen MT", 1, 12)); // NOI18N
+        jCheckBox1.setText("Show Password");
+        jCheckBox1.addActionListener(this::jCheckBox1ActionPerformed);
+
+        jButton1.setBackground(new java.awt.Color(141, 81, 19));
+        jButton1.setFont(new java.awt.Font("Tw Cen MT", 1, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("LOG IN");
+        jButton1.addActionListener(this::jButton1ActionPerformed);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(54, 54, 54)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jCheckBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 107, Short.MAX_VALUE))
+                    .addComponent(jPasswordField1)
+                    .addComponent(jTextField1))
+                .addGap(56, 56, 56))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(151, 151, 151)
+                .addComponent(jLabel2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(22, 22, 22)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 160, 420, 250));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/LogIn/OOP LAYOUT.png"))); // NOI18N
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1)
-        );
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -20, 1090, 570));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPasswordField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -76,6 +201,15 @@ public class LogInJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
